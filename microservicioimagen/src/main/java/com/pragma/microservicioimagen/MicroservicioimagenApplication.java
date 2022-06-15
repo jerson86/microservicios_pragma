764fb1c609ec3617e47bf49dc9fb8e8a33980083
jerson garcia <jersonjavier@gmail.com>
2022-06-14 19:32:19 -0500

    implementacion del cliente de eureka y config

--- microservicioimagen/src/main/java/com/pragma/microservicioimagen/MicroservicioimagenApplication.java
+++ microservicioimagen/src/main/java/com/pragma/microservicioimagen/MicroservicioimagenApplication.java
@@ -2,9 +2,10 @@ package com.pragma.microservicioimagen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicioimagenApplication {
 
